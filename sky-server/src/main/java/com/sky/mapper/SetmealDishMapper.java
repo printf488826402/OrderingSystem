1250163459862da127f3eb6fd60adebaf615a8e2
printf488826402 <17352720986@163.com>
2025-08-06 12:12:53 +0800

    店铺营业状态设置

---
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -0,0 +1,16 @@
+package com.sky.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface SetmealDishMapper {
+    /**
+     *
+     * @param dishIds
+     * @return
+     */
+    //select setmeal_id from setmeal_dish where dish_id in (?,?,?)
+    List<Long> getSetmealIdsByDishIds(List<Long> dishIds);
+}
