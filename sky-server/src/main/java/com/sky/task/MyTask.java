0d7d10ad79703d3ee24d09c8a5c57aaef9a5c835
printf488826402 <17352720986@163.com>
2025-08-14 15:04:02 +0800

    用户支付，商家待派送

---
+++ sky-server/src/main/java/com/sky/task/MyTask.java
@@ -0,0 +1,17 @@
+package com.sky.task;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * 自定义定时任务类
+ */
+@Component
+@Slf4j
+public class MyTask {
+    @Scheduled(cron = "0/5 * * ? ")
+    public void executetask(){
+        log.info("定时任务开始执行");
+    }
+}
